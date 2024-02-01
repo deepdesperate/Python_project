6a515da1da472e43f354ec07ccaaf091490e8e42
Naman Deep <namandeep444@gmail.com>
2024-02-01 22:32:49 +0530

    Animation Data properties execution

--- Ch7/addons/action_to_range.py
+++ Ch7/addons/action_to_range.py
@@ -5,7 +5,7 @@ bl_info = {
     "blender": (3, 00, 0),
     "location": "Timeline > View > Action to Scene Range",
     "description": "Action Duration to Scene Range",
-    "category"L "Learning",
+    "category": "Learning",
 }
 
 import bpy
@@ -17,3 +17,35 @@ class ActionToSceneRange(bpy.types.Operator):
     bl_description = "Transfer action range to scene range"
     bl_options = {'REGISTER','UNDO'}
     
+    use_preview: bpy.props.BoolProperty(default = True)
+
+    @classmethod
+    def poll(cls, context):
+        obj = context.object
+        if not obj:
+            return False
+        if not obj.animation_data:
+            return False
+        if not obj.animation_data.action:
+            return False
+        return True
+
+    def execute(self, context):
+        anim_data = context.object.animation_data
+        first, last = anim_data.action.frame_range
+        scn = context.scene
+        if self.use_preview:
+            scn.frame_preview_start = int(first)
+            scn.frame_preview_end = int(last)
+        else:
+            scn.frame_start = int(first)
+            scn.frame_end = int(last)
+        bpy.ops.action.view_all()
+        return {'FINISHED'}
+    
+def view_menu_items(self, context):
+    props = self.layout.operator( ActionToSceneRange.bl_idname, text = ActionToSceneRange.bl_idname + " (preview)")
+    props.use_preview = True
+
+    props = self.layout.operator( ActionToSceneRange.bl_idname)
+    props.use_preview = False
