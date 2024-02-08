9e33db5b0b0259ef25861ee89429f7c06c6b77c8
Naman Deep <namandeep444@gmail.com>
2024-02-08 22:42:04 +0530

    f_curve function for object animation curves

--- Ch8/addons/object_shaker.py
+++ Ch8/addons/object_shaker.py
@@ -25,4 +25,43 @@ class OBJECT_SHAKER(bpy.types.Operator):
         return context.object
     
     def execute(self, context):
+        if not context.object.animation_data:
+            anim = context.object.animation_data_create()
+        else:
+            anim = context.object.animation_data
+
+        if not anim.action:
+            action = bpy.data.actions.new('ShakeMotion')
+            anim.action = action
+        else:
+            action = anim.action
+
+        # Getting duration in terms of frames
+        # Creating animation before and after of current frame
+        fps = context.scene.render.fps
+        duration_frames = self.duration * fps / 2
+        current = context.scene.frame_current
+        start = current - duration_frames
+        end = current + duration_frames
+
+        #Getting fcurves for locationZ, rotation_euler X and rotation_euler Y
+        z_loc_crv = self.get_fcurve( context,'location', index = 2)
+        x_rot_crv = self.get_fcurve(context, 'rotation_euler', index = 0)
+        y_rot_crv = self.get_fcurve(context, 'rotation_euler' , index = 1)
+
+def get_fcurve(self, obj, data_path, index):
+    """Return F-Curve of given data_path/ index"""
+    action = obj.animation_data.action
+    # Halt the script, if for unforeseen reasons, no current action is found
+    assert action
+    try: 
+        crv = action.fcurves.new(data_path, index = index)
+    except RuntimeError:
+        # next can be applied to iterators as wella as arrays or collection
+        crv = next(fc for fc in action.fcurves if fc.data_path == data_path and fc.array_index == index)
         
+    # Making sure our fcurve has keyframes
+    if not crv.keyframe_points:
+        crv.keyframe_points.insert( frame = context.scene.frame_current, value = getattr(obj,data_path)[index])
+
+    return crv
