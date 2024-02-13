eb8ec6170d941bf76a51ec1b8ee511b4781d4f72
Naman Deep <namandeep444@gmail.com>
2024-02-13 22:17:34 +0530

    Punch clock initial commit

---
+++ Ch10/addons/punch_clock.py
@@ -0,0 +1,7 @@
+bl_info = {
+    "name": "Text PunchClock",
+    "author": "Naman Deep",
+    "version": (1, 0),
+    "blender": (3, 00, 0),
+    "description": "Create an Hour/Minutes text object",
+}
